9bd8e2d23cff12f4822848935e3e2883ae0762ed
mohamedkhaled22669 <51810133+mohamedkhaled22669@users.noreply.github.com>
2023-06-08 22:05:31 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
+// use express
+const app = express()
+
+// middleware
+app.use(express.json())
+
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method);
+//     next()
+// })
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // app listen for request
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT);
+        })
+
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
